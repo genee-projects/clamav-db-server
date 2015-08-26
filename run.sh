1e7e614a2a3bc2561400d778de1174cc93bbf9a1
Rui Ma <rui.ma@geneegroup.com>
2015-08-26 12:33:56 +0900

    修改 docker images 名称 修改 docker container 名称

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # 删除 rsync-serve
-docker rm -f rsync-server
+docker rm -f clamav-db-server
 
 # 构建
-docker build -t 'genee/rsync-server' docker
+docker build -t 'genee/clamav-db-server' docker
 
 # 运行服务
-docker run -d --name=rsync-server -p 873:873 -v $(pwd)/clamav/:/var/lib/clamav/ genee/rsync-server
+docker run -d --name=clamav-db-server -p 873:873 -v $(pwd)/clamav/:/var/lib/clamav/ genee/clamav-db-server
 
 echo "done!"

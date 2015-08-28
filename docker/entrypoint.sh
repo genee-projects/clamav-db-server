582ad610532b67bfa3f8ca6eccd5322cfe549dd6
Rui Ma <rui.ma@geneegroup.com>
2015-08-28 10:26:41 +0900

    修改无法启动问题, 修改 docker 引发的问题

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
+rm /var/run/rsync.pid
 exec start-stop-daemon --start --pidfile /var/run/rsync.pid --make-pidfile --exec /usr/bin/rsync -- --no-detach --daemon --config /etc/rsyncd.conf
-
